b94b8d92442ada909251c791864de243ece0becc
wincss <wincss@gmail.com>
2020-08-03 18:17:47 +0800

    impl display

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,20 @@ pub struct TreeNode {
     pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
+impl Display for TreeNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "[{}]",
+            self.to_vec()
+                .into_iter()
+                .map(|v| v.map_or("null".to_string(), |v| format!("{}", v)))
+                .collect::<Vec<String>>()
+                .join(", ")
+        )
+    }
+}
+
 impl TreeNode {
     #[inline]
     pub fn new(val: i32) -> Self {
@@ -34,32 +48,55 @@ impl TreeNode {
             right: None,
         }
     }
-}
 
-pub fn to_tree(list: &[Option<i32>]) -> Option<Rc<RefCell<TreeNode>>> {
-    let mut list: VecDeque<Option<i32>> = VecDeque::from_iter(list.to_vec());
-    let mut queue = VecDeque::new();
-    let mut root = None;
-    while !list.is_empty() {
-        if root.is_none() {
-            let node = Rc::new(RefCell::new(TreeNode::new(
-                list.pop_front().unwrap().expect("root must not null"),
-            )));
-            root.replace(Rc::clone(&node));
-            queue.push_back(Rc::clone(&node));
-            continue;
+    pub fn to_vec(&self) -> Vec<Option<i32>> {
+        let mut output = Vec::new();
+        let mut queue = VecDeque::new();
+        output.push(Some(self.val));
+        queue.push_back(self.left.as_ref().and_then(|v| Some(Rc::clone(&v))));
+        queue.push_back(self.right.as_ref().and_then(|v| Some(Rc::clone(&v))));
+        while let Some(v) = queue.pop_front() {
+            output.push(match v {
+                None => None,
+                Some(node) => {
+                    let node = node.borrow();
+                    queue.push_back(node.left.as_ref().and_then(|v| Some(Rc::clone(v))));
+                    queue.push_back(node.right.as_ref().and_then(|v| Some(Rc::clone(v))));
+                    Some(node.val)
+                }
+            })
         }
-        let current = queue.pop_front().expect("too many items");
-        if let Some(v) = list.pop_front().unwrap() {
-            let node = Rc::new(RefCell::new(TreeNode::new(v)));
-            current.borrow_mut().left.replace(Rc::clone(&node));
-            queue.push_back(Rc::clone(&node));
+        while let Some(None) = output.last() {
+            output.pop();
         }
-        if let Some(v) = list.pop_front().flatten() {
-            let node = Rc::new(RefCell::new(TreeNode::new(v)));
-            current.borrow_mut().right.replace(Rc::clone(&node));
-            queue.push_back(Rc::clone(&node));
-        }
+        output
     }
-    root
+
+    pub fn from_array(list: &[Option<i32>]) -> Option<Rc<RefCell<Self>>> {
+        let mut list: VecDeque<Option<i32>> = VecDeque::from_iter(list.to_vec());
+        let mut queue = VecDeque::new();
+        let mut root = None;
+        while !list.is_empty() {
+            if root.is_none() {
+                let node = Rc::new(RefCell::new(Self::new(
+                    list.pop_front().unwrap().expect("root must not null"),
+                )));
+                root.replace(Rc::clone(&node));
+                queue.push_back(Rc::clone(&node));
+                continue;
+            }
+            let current = queue.pop_front().expect("too many items");
+            if let Some(v) = list.pop_front().unwrap() {
+                let node = Rc::new(RefCell::new(Self::new(v)));
+                current.borrow_mut().left.replace(Rc::clone(&node));
+                queue.push_back(Rc::clone(&node));
+            }
+            if let Some(v) = list.pop_front().flatten() {
+                let node = Rc::new(RefCell::new(Self::new(v)));
+                current.borrow_mut().right.replace(Rc::clone(&node));
+                queue.push_back(Rc::clone(&node));
+            }
+        }
+        root
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,16 @@ fn main() {
         "minimal_steps = {}",
         Solution::minimal_steps(to_string_vec(&["S#O", "M..", "M.T"]))
     );
-    let tree = to_tree(&[Some(1), None, Some(2), Some(3), Some(4)]);
-    println!("to_tree = {:?}", tree);
+    let tree = TreeNode::from_array(&[Some(1), None, Some(2), Some(3), Some(4)]);
+    println!("to_tree = {}", tree.as_ref().unwrap().borrow());
     let mut inv_tree = Solution::invert_tree(tree);
+    println!("inv_tree = {}", inv_tree.as_ref().unwrap().borrow());
+    Solution::flatten(&mut inv_tree);
+    println!("after flatten = {}", inv_tree.as_ref().unwrap().borrow());
+
+    let empty_tree = TreeNode::from_array(&[]);
+    println!("to_tree = {:?}", empty_tree);
+    let mut inv_tree = Solution::invert_tree(empty_tree);
     println!("inv_tree = {:?}", inv_tree);
     Solution::flatten(&mut inv_tree);
     println!("after flatten = {:?}", inv_tree);

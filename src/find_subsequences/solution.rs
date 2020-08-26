2c61907c53b056547298ed4e1d308dc7b5ee7871
wincss <wincss@gmail.com>
2020-08-26 17:38:41 +0800

    optimize

--- src/find_subsequences/solution.rs
+++ src/find_subsequences/solution.rs
@@ -1,46 +1,26 @@
 use crate::*;
 
-use std::collections::{HashMap, HashSet};
 impl Solution {
     pub fn find_subsequences(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        fn helper(
-            nums: &Vec<i32>,
-            i: usize,
-            big: Option<usize>,
-            save: &mut HashMap<(usize, Option<usize>), HashSet<Vec<i32>>>,
-        ) -> HashSet<Vec<i32>> {
-            let n = nums.len();
-            if n == 0 {
-                return HashSet::new();
+        fn helper(nums: &Vec<i32>, i: usize, current: &mut Vec<i32>, result: &mut Vec<Vec<i32>>) {
+            if i == nums.len() {
+                if current.len() > 1 {
+                    result.push(current.clone());
+                }
+                return;
             }
-            if i == n {
-                let mut h = HashSet::new();
-                h.insert(vec![]);
-                return h;
+            if current.len() == 0 || nums[i] >= current[current.len() - 1] {
+                current.push(nums[i]);
+                helper(nums, i + 1, current, result);
+                current.pop();
             }
-            if let Some(ans) = save.get(&(i, big)) {
-                return ans.clone();
+            if current.len() == 0 || nums[i] != current[current.len() - 1] {
+                helper(nums, i + 1, current, result);
             }
-            let mut ans = helper(nums, i + 1, big, save);
-            if big.is_none() || nums[i] >= nums[big.unwrap()] {
-                let next = helper(nums, i + 1, Some(i), save)
-                    .iter()
-                    .map(|v| {
-                        vec![nums[i]]
-                            .into_iter()
-                            .chain(v.clone().into_iter())
-                            .collect()
-                    })
-                    .collect::<HashSet<Vec<i32>>>();
-                ans.extend(next);
-            }
-            save.insert((i, big), ans.clone());
-            ans
         }
-        let mut save = HashMap::new();
-        helper(&nums, 0, None, &mut save)
-            .into_iter()
-            .filter(|v| v.len() >= 2)
-            .collect()
+        let mut result = vec![];
+        let mut current = vec![];
+        helper(&nums, 0, &mut current, &mut result);
+        result
     }
 }

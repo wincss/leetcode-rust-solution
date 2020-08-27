05ae4f6a5d926b2727149e9e24b878e574ff01d1
nixiaoxi <wincss@gmail.com>
2020-08-28 00:10:18 +0800

    optimize

--- src/find_itinerary/solution.rs
+++ src/find_itinerary/solution.rs
@@ -1,46 +1,27 @@
 use crate::*;
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 impl Solution {
     pub fn find_itinerary(tickets: Vec<Vec<String>>) -> Vec<String> {
-        let n = tickets.len();
         let mut edges = HashMap::new();
-        for (i, mut edge) in tickets.into_iter().enumerate() {
-            let source = edges.entry(edge.remove(0)).or_insert(Vec::new());
-            source.push((edge.pop().unwrap(), i));
+        for edge in tickets.into_iter() {
+            let source = edges.entry(edge[0].clone()).or_insert(Vec::new());
+            source.push(edge[1].clone());
         }
         for (_, destinations) in edges.iter_mut() {
             destinations.sort();
+            destinations.reverse();
         }
-        let mut current = vec![String::from("JFK")];
-        let mut result = None;
-        fn helper(
-            left_tickets: &mut HashSet<usize>,
-            edges: &HashMap<String, Vec<(String, usize)>>,
-            current: &mut Vec<String>,
-            result: &mut Option<Vec<String>>,
-        ) {
-            if left_tickets.len() == 0 {
-                result.replace(current.clone());
-                return;
+        // println!("{:?}", edges);
+        fn helper(now: String, edges: &mut HashMap<String, Vec<String>>, result: &mut Vec<String>) {
+            while let Some(destination) = edges.get_mut(&now).and_then(|v| v.pop()) {
+                helper(destination, edges, result);
             }
-            if let Some(destinations) = edges.get(current.last().unwrap()) {
-                for (destination, i) in destinations {
-                    if left_tickets.remove(i) {
-                        current.push(destination.clone());
-                        helper(left_tickets, edges, current, result);
-                        if result.is_some() {
-                            return;
-                        }
-                        left_tickets.insert(*i);
-                        current.pop();
-                    }
-                }
-            }
+            result.push(now);
         }
-        let mut left_tickets = (0..n).collect();
-        helper(&mut left_tickets, &edges, &mut current, &mut result);
-        result.unwrap_or(vec![])
+        let mut result = vec![];
+        helper(String::from("JFK"), &mut edges, &mut result);
+        result.into_iter().rev().collect()
     }
 }

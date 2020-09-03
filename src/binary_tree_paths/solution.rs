8fa8157d3131e33de7ddd5c4daeebf8f1a62cdac
wincss <wincss@gmail.com>
2020-09-04 01:00:16 +0800

    refactor

--- src/binary_tree_paths/solution.rs
+++ src/binary_tree_paths/solution.rs
@@ -18,17 +18,10 @@ impl Solution {
             current.push(node.val.to_string());
             if node.left.is_none() && node.right.is_none() {
                 result.push(current.join("->"));
+            } else {
+                walk(node.left.as_ref().map(Rc::clone), current, result);
+                walk(node.right.as_ref().map(Rc::clone), current, result);
             }
-            walk(
-                node.left.as_ref().and_then(|v| Some(Rc::clone(&v))),
-                current,
-                result,
-            );
-            walk(
-                node.right.as_ref().and_then(|v| Some(Rc::clone(&v))),
-                current,
-                result,
-            );
             current.pop();
         }
         walk(root, &mut current, &mut result);

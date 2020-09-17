b79e15c13f3e2d9bd9c769449a6085a413c0c89b
wincss <wincss@gmail.com>
2020-09-17 18:03:46 +0800

    add find_redundant_directed_connection (685)

--- src/find_redundant_directed_connection/solution.rs
+++ src/find_redundant_directed_connection/solution.rs
@@ -1,41 +1,65 @@
 use crate::*;
 
-use std::collections::VecDeque;
+use std::collections::HashMap;
 
 impl Solution {
     pub fn find_redundant_directed_connection(edges: Vec<Vec<i32>>) -> Vec<i32> {
+        fn find(x: i32, parent: &mut HashMap<i32, i32>, size: &mut HashMap<i32, i32>) -> i32 {
+            let f = parent.entry(x).or_insert(x);
+            size.entry(x).or_insert(1);
+            if *f == x {
+                x
+            } else {
+                let p = find(parent[&x], parent, size);
+                parent.insert(x, p);
+                parent[&x]
+            }
+        }
+        fn union(
+            x: i32,
+            y: i32,
+            parent: &mut HashMap<i32, i32>,
+            size: &mut HashMap<i32, i32>,
+        ) -> bool {
+            let x = find(x, parent, size);
+            let y = find(y, parent, size);
+            if x == y {
+                return false;
+            }
+            if size[&x] > size[&y] {
+                parent.insert(y, x);
+                size.insert(x, size[&x] + size[&y]);
+            } else {
+                parent.insert(x, y);
+                size.insert(y, size[&x] + size[&y]);
+            }
+            true
+        }
         let n = edges.len();
-        let mut indegree = vec![0; n + 1];
-        let mut has_indegree_2 = false;
-        let mut e = vec![vec![]; n + 1];
+        let mut parent = HashMap::new();
+        let mut size = HashMap::new();
+        let mut inedge = vec![0; n + 1];
+
+        let mut collision = None;
+        let mut cycle = None;
 
         for edge in edges.iter() {
-            indegree[edge[1] as usize] += 1;
-            if indegree[edge[1] as usize] == 2 {
-                has_indegree_2 = true;
-            }
-            e[edge[0] as usize].push(edge[1] as usize);
-        }
-        let mut start = edges[0][0] as usize;
-        if has_indegree_2 {
-            for i in 1..n {
-                if indegree[i] == 0 {
-                    start = i;
+            if inedge[edge[1] as usize] > 0 {
+                collision = Some(edge.clone());
+            } else {
+                inedge[edge[1] as usize] = edge[0];
+                if !union(edge[0], edge[1], &mut parent, &mut size) {
+                    cycle = Some(edge.clone());
                 }
             }
-        };
-        let mut queue = VecDeque::new();
-        queue.push_back(start);
-        indegree[start] = -1;
-        while let Some(v) = queue.pop_front() {
-            for &d in e[v].iter() {
-                if indegree[d] < 0 {
-                    return vec![v as i32, d as i32];
-                }
-                indegree[d] = indegree[v] - 1;
-                queue.push_back(d);
-            }
         }
-        unreachable!()
+        if collision.is_none() {
+            cycle.unwrap()
+        } else if cycle.is_none() {
+            collision.unwrap()
+        } else {
+            let collision = collision.unwrap();
+            vec![inedge[collision[1] as usize], collision[1]]
+        }
     }
 }

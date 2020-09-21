e732fcbba6e4b27ea7af37c5e370e2d8face0ca4
wincss <wincss@gmail.com>
2020-09-21 22:22:20 +0800

    optimize

--- src/connect_two_groups/solution.rs
+++ src/connect_two_groups/solution.rs
@@ -4,29 +4,31 @@ impl Solution {
     pub fn connect_two_groups(cost: Vec<Vec<i32>>) -> i32 {
         let n = cost.len();
         let m = cost[0].len();
-        let mut cost_sum = vec![vec![0; 1 << m]; n];
-        for i in 0..n {
-            for j in 0..1 << m {
-                for k in 0..m {
-                    if j & (1 << k) > 0 {
-                        cost_sum[i][j] += cost[i][k];
-                    }
-                }
-            }
-        }
-        // println!("{:?}", cost_sum);
         let mut dp = vec![vec![]; 2];
-        dp[0] = cost_sum[0].clone();
-        for i in 1..n {
+        dp[0] = vec![std::i32::MAX; 1 << m];
+        dp[0][0] = 0;
+        for i in 1..=n {
             dp[i % 2] = vec![std::i32::MAX; 1 << m];
             for j in 1..1 << m {
-                for k in 1..1 << m {
-                    dp[i % 2][j | k] =
-                        std::cmp::min(dp[i % 2][j | k], dp[1 - i % 2][k] + cost_sum[i][j]);
+                for k in 0..m {
+                    if (1 << k) & j > 0 {
+                        dp[i % 2][j] = std::cmp::min(
+                            dp[i % 2][j],
+                            dp[1 - i % 2][j].saturating_add(cost[i - 1][k]),
+                        );
+                        dp[i % 2][j] = std::cmp::min(
+                            dp[i % 2][j],
+                            dp[i % 2][j ^ (1 << k)].saturating_add(cost[i - 1][k]),
+                        );
+                        dp[i % 2][j] = std::cmp::min(
+                            dp[i % 2][j],
+                            dp[1 - i % 2][j ^ (1 << k)].saturating_add(cost[i - 1][k]),
+                        );
+                    }
                 }
             }
             // println!("{}: {:?}", i, dp[i % 2]);
         }
-        dp[(n - 1) % 2][(1 << m) - 1]
+        dp[n % 2][(1 << m) - 1]
     }
 }

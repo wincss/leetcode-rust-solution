6a93f8814c129d7613b4ab2ee1b2e1da33d6d55b
wincss <wincss@gmail.com>
2020-09-23 04:24:49 +0800

    refactor: remove unnecessary Option

--- src/merge_k_lists/solution.rs
+++ src/merge_k_lists/solution.rs
@@ -6,36 +6,26 @@ impl Solution {
         fn merge(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
             let mut head = Box::new(ListNode::new(0));
 
-            let mut next = Some(&mut head);
+            let mut next = &mut head;
             let mut l1 = l1;
             let mut l2 = l2;
-            while l1.is_some() || l2.is_some() {
-                if l1.is_none() {
-                    next.unwrap().next = l2;
-                    break;
-                }
-                if l2.is_none() {
-                    next.unwrap().next = l1;
-                    break;
-                }
+            while l1.is_some() && l2.is_some() {
                 let mut v1 = l1.take().unwrap();
                 let mut v2 = l2.take().unwrap();
                 if v1.val < v2.val {
                     l1 = v1.next.take();
                     l2.replace(v2);
 
-                    let new = next.take().unwrap();
-                    new.next = Some(v1);
-                    next = new.next.as_mut();
+                    next.next = Some(v1);
                 } else {
                     l2 = v2.next.take();
                     l1.replace(v1);
 
-                    let new = next.take().unwrap();
-                    new.next = Some(v2);
-                    next = new.next.as_mut();
+                    next.next = Some(v2);
                 }
+                next = next.next.as_mut().unwrap();
             }
+            next.next = l1.xor(l2);
             head.next
         }
         let mut lists: VecDeque<Option<Box<ListNode>>> = lists.into_iter().collect();

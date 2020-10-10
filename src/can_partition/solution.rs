9d1bb9cbcaa10ec4f2e3123ef9e678655110d081
wincss <wincss@gmail.com>
2020-10-11 02:08:24 +0800

    optimize

--- src/can_partition/solution.rs
+++ src/can_partition/solution.rs
@@ -10,14 +10,20 @@ impl Solution {
             return false;
         }
         sum >>= 1;
+        let sum = sum as usize;
         let n = nums.len();
-        let mut dp = vec![vec![false; sum as usize + 1]; n + 1];
-        dp[n][0] = true;
-        for i in (0..n).rev() {
-            for j in 0..=sum as usize {
-                dp[i][j] = dp[i + 1][j] || (j as i32 >= nums[i] && dp[i + 1][j - nums[i] as usize])
+        let mut dp = vec![false; sum + 1];
+        dp[sum] = true;
+        for i in 0..n {
+            for j in 0..=sum {
+                if j >= nums[i] as usize {
+                    dp[j - nums[i] as usize] |= dp[j]
+                }
+            }
+            if dp[0] {
+                break;
             }
         }
-        dp[0][sum as usize]
+        dp[0]
     }
 }

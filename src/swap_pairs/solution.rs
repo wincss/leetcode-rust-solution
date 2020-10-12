779645ee80d8fc70b8d2ed6f191ad275b3ba159e
wincss <wincss@gmail.com>
2020-10-13 01:44:49 +0800

    refactor

--- src/swap_pairs/solution.rs
+++ src/swap_pairs/solution.rs
@@ -6,13 +6,16 @@ impl Solution {
         root.next = head;
         let mut root = Box::new(root);
         let mut this = &mut root;
-        while this.next.is_some() && this.next.as_ref().unwrap().next.is_some() {
-            let mut a = this.next.take().unwrap();
-            let mut b = a.next.take().unwrap();
-            a.next = b.next.take();
-            b.next.replace(a);
-            this.next.replace(b);
-            this = this.next.as_mut().unwrap().next.as_mut().unwrap();
+        while let Some(mut a) = this.next.take() {
+            if let Some(mut b) = a.next.take() {
+                a.next = b.next.take();
+                b.next.replace(a);
+                this.next.replace(b);
+                this = this.next.as_mut().unwrap().next.as_mut().unwrap();
+            } else {
+                this.next.replace(a);
+                break;
+            }
         }
         root.next
     }

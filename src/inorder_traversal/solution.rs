a7d5467900ac8178a1ccb5a27e076ae2db3af86b
wincss <wincss@gmail.com>
2020-10-27 00:26:22 +0800

    optimize

--- src/inorder_traversal/solution.rs
+++ src/inorder_traversal/solution.rs
@@ -4,21 +4,17 @@ use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-        Self::inorder_traversal_recursion(root)
+        let mut result = vec![];
+        Self::inorder_traversal_recursion(root, &mut result);
+        result
     }
-    pub fn inorder_traversal_recursion(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    pub fn inorder_traversal_recursion(root: Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>) {
         if root.is_none() {
-            return vec![];
+            return;
         }
-        let mut ans = vec![];
         let root = root.as_ref().unwrap().borrow();
-        ans.extend(Self::inorder_traversal_recursion(
-            root.left.as_ref().map(Rc::clone),
-        ));
-        ans.push(root.val);
-        ans.extend(Self::inorder_traversal_recursion(
-            root.right.as_ref().map(Rc::clone),
-        ));
-        ans
+        Self::inorder_traversal_recursion(root.left.as_ref().map(Rc::clone), result);
+        result.push(root.val);
+        Self::inorder_traversal_recursion(root.right.as_ref().map(Rc::clone), result);
     }
 }

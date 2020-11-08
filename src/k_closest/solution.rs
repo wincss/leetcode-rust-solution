516e9d3d0fe781e83715e55112bfa64795357f72
wincss <wincss@gmail.com>
2020-11-09 01:03:32 +0800

    refactor: simplify code

--- src/k_closest/solution.rs
+++ src/k_closest/solution.rs
@@ -4,6 +4,6 @@ impl Solution {
     pub fn k_closest(points: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
         let mut points = points;
         points.sort_by_key(|v| v[0] * v[0] + v[1] * v[1]);
-        points[0..k as usize].iter().map(|v| v.clone()).collect()
+        points.into_iter().take(k as usize).collect()
     }
 }

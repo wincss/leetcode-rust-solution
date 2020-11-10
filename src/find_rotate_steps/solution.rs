64c1bfe34fb01ad46d00eba2e2487c89a728f49c
wincss <wincss@gmail.com>
2020-11-11 01:49:00 +0800

    refactor: simplify code

--- src/find_rotate_steps/solution.rs
+++ src/find_rotate_steps/solution.rs
@@ -4,21 +4,19 @@ use std::collections::HashMap;
 impl Solution {
     pub fn find_rotate_steps(ring: String, key: String) -> i32 {
         let n = ring.len();
-        let m = key.len();
         let ring: Vec<char> = ring.chars().collect();
-        let key: Vec<char> = key.chars().collect();
-
         let mut ring_char = HashMap::new();
         for (i, &v) in ring.iter().enumerate() {
             ring_char.entry(v).or_insert(vec![]).push(i);
         }
+
         let mut dp = vec![vec![0; n]; 2];
-        for i in (0..m).rev() {
+        for (i, c) in key.char_indices().rev() {
             for j in 0..n {
-                dp[i & 1][j] = if ring[j] == key[i] {
+                dp[i & 1][j] = if ring[j] == c {
                     dp[i & 1 ^ 1][j]
                 } else {
-                    ring_char[&key[i]]
+                    ring_char[&c]
                         .iter()
                         .map(|&k| {
                             dp[i & 1 ^ 1][k]

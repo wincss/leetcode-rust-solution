ba2dbbe1b9906b8fe52f278fa0f6980c619eb74c
wincss <wincss@gmail.com>
2020-12-03 01:24:27 +0800

    optimize

--- src/count_primes/solution.rs
+++ src/count_primes/solution.rs
@@ -3,15 +3,22 @@ use crate::*;
 impl Solution {
     pub fn count_primes(n: i32) -> i32 {
         let n = n as usize;
-        let mut prime = vec![true; n];
+        let mut is_prime = vec![true; n];
+        let mut primes = vec![];
         let mut count = 0;
         for i in 2..n {
-            if !prime[i] {
-                continue;
+            if is_prime[i] {
+                count += 1;
+                primes.push(i);
             }
-            count += 1;
-            for j in 2..=(n - 1) / i {
-                prime[i * j] = false;
+            for &j in primes.iter() {
+                if i * j >= n {
+                    break;
+                }
+                is_prime[i * j] = false;
+                if i % j == 0 {
+                    break;
+                }
             }
         }
         count

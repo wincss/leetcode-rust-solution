710d4b93e41dfc489667e35262c6da247f151d10
wincss <wincss@gmail.com>
2021-01-08 02:27:06 +0800

    optimize: another way

--- src/rotate/solution.rs
+++ src/rotate/solution.rs
@@ -1,6 +1,31 @@
 use crate::*;
 
 impl Solution {
+    pub fn rotate_by_reverse(nums: &mut Vec<i32>, k: i32) {
+        fn reverse(nums: &mut Vec<i32>, from: usize, to: usize) {
+            let mut from = from;
+            let mut to = to;
+            while from < to {
+                let t = nums[from];
+                nums[from] = nums[to];
+                nums[to] = t;
+                from += 1;
+                to -= 1;
+            }
+        }
+        let n = nums.len();
+        if n == 0 {
+            return;
+        }
+        let k = k as usize % n;
+        if k == 0 {
+            return;
+        }
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
     pub fn rotate(nums: &mut Vec<i32>, k: i32) {
         let n = nums.len();
         if n == 0 {

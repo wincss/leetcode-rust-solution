af478913a15600e8240e2a2f54020fc18849c5e3
wincss <wincss@gmail.com>
2021-01-21 18:44:35 +0800

    remove redundant code

--- src/accounts_merge/solution.rs
+++ src/accounts_merge/solution.rs
@@ -1,44 +1,11 @@
 use crate::*;
 
 use std::collections::{HashMap, HashSet};
+
+use common::algorithms::union_find::*;
+
 impl Solution {
     pub fn accounts_merge(accounts: Vec<Vec<String>>) -> Vec<Vec<String>> {
-        fn find<'a, T>(
-            x: &'a T,
-            parent: &mut HashMap<&'a T, &'a T>,
-            size: &mut HashMap<&'a T, usize>,
-        ) -> &'a T
-        where
-            T: std::cmp::Eq + std::hash::Hash,
-        {
-            let f = parent.entry(x).or_insert(x);
-            size.entry(x).or_insert(1);
-            if **f == *x {
-                x
-            } else {
-                let p = find(*f, parent, size);
-                parent.insert(x, p);
-                p
-            }
-        }
-        fn union<'a, T>(
-            x: &'a T,
-            y: &'a T,
-            parent: &mut HashMap<&'a T, &'a T>,
-            size: &mut HashMap<&'a T, usize>,
-        ) where
-            T: std::cmp::Eq + std::hash::Hash,
-        {
-            let x = find(x, parent, size);
-            let y = find(y, parent, size);
-            if size[&x] > size[&y] {
-                parent.insert(y, x);
-                size.insert(x, size[&x] + size[&y]);
-            } else {
-                parent.insert(x, y);
-                size.insert(y, size[&x] + size[&y]);
-            }
-        }
         let mut parent = HashMap::new();
         let mut size = HashMap::new();
         let mut mail_to_name = HashMap::new();
@@ -46,7 +13,7 @@ impl Solution {
             let n = item.len();
             mail_to_name.insert(&item[1], &item[0]);
             for i in 2..n {
-                union(&item[1], &item[i], &mut parent, &mut size);
+                union(&&item[1], &&item[i], &mut parent, &mut size);
             }
         }
         let mut mailgroup = HashMap::new();
@@ -54,7 +21,7 @@ impl Solution {
         for item in accounts.iter() {
             let n = item.len();
             for i in 1..n {
-                let group = find(&item[i], &mut parent, &mut size);
+                let group = find(&&item[i], &mut parent, &mut size);
                 mailgroup
                     .entry(group)
                     .or_insert(HashSet::new())

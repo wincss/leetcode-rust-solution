70268083dfa299dd0ccf752ed2133a0587155e33
wincss <wincss@gmail.com>
2021-03-26 14:57:48 +0800

    add delete_duplicates (83)

--- src/delete_duplicates/solution.rs
+++ src/delete_duplicates/solution.rs
@@ -2,6 +2,24 @@ use crate::*;
 
 impl Solution {
     pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut root = Some(Box::new(ListNode::new(0)));
+        let mut tail = &mut root;
+        let mut last = None;
+        let mut head = head;
+        while let Some(mut node) = head.take() {
+            head = node.next.take();
+            if last.is_some() && last.unwrap() == node.val {
+                continue;
+            }
+
+            last = Some(node.val);
+            tail.as_mut().unwrap().next = Some(node);
+            tail = &mut tail.as_mut().unwrap().next;
+        }
+        root.unwrap().next
+    }
+
+    pub fn delete_duplicates_ii(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let mut root = ListNode::new(0);
         root.next = head;
         let mut root = Box::new(root);
--- src/delete_duplicates/tests.rs
+++ src/delete_duplicates/tests.rs
@@ -3,7 +3,7 @@ use crate::*;
 #[test]
 fn case_1() {
     assert_eq!(
-        Solution::delete_duplicates(ListNode::from_array(&[1, 2, 3, 3, 4, 4, 5])),
+        Solution::delete_duplicates_ii(ListNode::from_array(&[1, 2, 3, 3, 4, 4, 5])),
         ListNode::from_array(&[1, 2, 5])
     );
 }
@@ -11,7 +11,23 @@ fn case_1() {
 #[test]
 fn case_2() {
     assert_eq!(
-        Solution::delete_duplicates(ListNode::from_array(&[1, 1, 1, 2, 3])),
+        Solution::delete_duplicates_ii(ListNode::from_array(&[1, 1, 1, 2, 3])),
         ListNode::from_array(&[2, 3])
+    );
+}
+
+#[test]
+fn case_3() {
+    assert_eq!(
+        Solution::delete_duplicates(ListNode::from_array(&[1, 1, 2])),
+        ListNode::from_array(&[1, 2])
+    );
+}
+
+#[test]
+fn case_4() {
+    assert_eq!(
+        Solution::delete_duplicates(ListNode::from_array(&[1, 1, 2, 3, 3])),
+        ListNode::from_array(&[1, 2, 3])
     );
 }

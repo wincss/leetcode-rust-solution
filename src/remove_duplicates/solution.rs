8d8b2a0c9ae822df43e3ad3d38e75506d48a91c4
wincss <wincss@gmail.com>
2021-04-06 15:59:49 +0800

    add remove_duplicates (80)

--- src/remove_duplicates/solution.rs
+++ src/remove_duplicates/solution.rs
@@ -1,7 +1,29 @@
 use crate::*;
 
 impl Solution {
-    pub fn remove_duplicates(s: String) -> String {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut last = None;
+        let mut last_count = 0;
+        let mut idx = 0;
+        let n = nums.len();
+        for i in 0..n {
+            if last == Some(nums[i]) && last_count >= 2 {
+                continue;
+            }
+            nums[idx] = nums[i];
+            idx += 1;
+            if last == Some(nums[i]) {
+                last_count += 1;
+            } else {
+                last_count = 1;
+                last = Some(nums[i]);
+            }
+        }
+        idx as i32
+    }
+
+    // remove_duplicates in origin problem
+    pub fn remove_duplicates_string(s: String) -> String {
         let mut st = vec![];
         for c in s.chars() {
             if *st.last().unwrap_or(&'*') == c {
--- src/remove_duplicates/tests.rs
+++ src/remove_duplicates/tests.rs
@@ -3,7 +3,21 @@ use crate::*;
 #[test]
 fn case_1() {
     assert_eq!(
-        Solution::remove_duplicates(String::from("abbaca")),
+        Solution::remove_duplicates_string(String::from("abbaca")),
         String::from("ca")
     );
+}
+
+#[test]
+fn case_2() {
+    let mut input = vec![1, 1, 1, 2, 2, 3];
+    let length = Solution::remove_duplicates(&mut input);
+    assert_eq!(&input[0..length as usize], &[1, 1, 2, 2, 3]);
+}
+
+#[test]
+fn case_3() {
+    let mut input = vec![0, 0, 1, 1, 1, 1, 2, 3, 3];
+    let length = Solution::remove_duplicates(&mut input);
+    assert_eq!(&input[0..length as usize], &[0, 0, 1, 1, 2, 3, 3]);
 }

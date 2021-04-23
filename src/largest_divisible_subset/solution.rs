4e52b9ce5859ae1bf526273ea3ed53ad5ec4bd96
wincss <wincss@gmail.com>
2021-04-23 16:54:15 +0800

    refactor: remove vector clone

--- src/largest_divisible_subset/solution.rs
+++ src/largest_divisible_subset/solution.rs
@@ -8,18 +8,31 @@ impl Solution {
         }
         let mut nums = nums;
         nums.sort();
+        let mut max_idx = 0;
         let mut dp = vec![];
         for i in 0..n {
-            dp.push(vec![nums[i]]);
+            dp.push((1, 0));
             for j in (0..i).rev() {
                 if nums[i] % nums[j] == 0 {
-                    if dp[j].len() >= dp[i].len() {
-                        dp[i] = dp[j].clone();
-                        dp[i].push(nums[i]);
+                    if dp[j].0 >= dp[i].0 {
+                        dp[i] = (dp[j].0 + 1, j);
                     }
                 }
             }
+            if dp[i].0 > dp[max_idx].0 {
+                max_idx = i;
+            }
         }
-        dp.into_iter().max_by_key(|v| v.len()).unwrap()
+        // println!("{:?}", dp);
+
+        let mut result = vec![];
+        loop {
+            result.push(nums[max_idx]);
+            if dp[max_idx].0 == 1 {
+                break;
+            }
+            max_idx = dp[max_idx].1;
+        }
+        result
     }
 }

8b1d8cc2e1bc527757ec7f7084c021a078097383
wincss <wincss@gmail.com>
2021-05-01 00:49:56 +0800

    refactor: use bitwise operator

--- src/single_number/solution.rs
+++ src/single_number/solution.rs
@@ -2,20 +2,18 @@ use crate::*;
 
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        let mut b = vec![0; 32];
-        for i in nums.into_iter() {
-            for j in 0..32 {
-                if i & (1 << j) != 0 {
-                    b[j] += 1;
-                }
-            }
+        let mut t0 = -1;
+        let mut t1 = 0;
+        let mut t2 = 0;
+        for i in nums {
+            let v0 = t0 & i;
+            let v1 = t1 & i;
+            let v2 = t2 & i;
+
+            t2 = (t2 ^ v2) | v1;
+            t1 = (t1 ^ v1) | v0;
+            t0 = (t0 ^ v0) | v2;
         }
-        let mut result = 0;
-        for j in 0..32 {
-            if b[j] % 3 > 0 {
-                result += 1 << j;
-            }
-        }
-        result
+        t1
     }
 }

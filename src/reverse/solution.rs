79dc54d2fcf8860a22fc0d56c76f577e9bb7af80
wincss <wincss@gmail.com>
2021-05-03 00:08:41 +0800

    refactor: simplify code

--- src/reverse/solution.rs
+++ src/reverse/solution.rs
@@ -5,13 +5,9 @@ impl Solution {
         let mut ans: i32 = 0;
         let mut x = x;
         while x != 0 {
-            if let Some(v) = ans.checked_mul(10) {
-                if let Some(v2) = v.checked_add(x % 10) {
-                    x = x / 10;
-                    ans = v2;
-                } else {
-                    return 0;
-                }
+            if let Some(v) = ans.checked_mul(10).and_then(|v| v.checked_add(x % 10)) {
+                x = x / 10;
+                ans = v;
             } else {
                 return 0;
             }

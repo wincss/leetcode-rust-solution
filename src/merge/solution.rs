744b1425b3c9a5c617dc757f7aa12671c2da4fca
wincss <wincss@gmail.com>
2021-05-07 00:08:17 +0800

    add merge (56)

--- src/merge/solution.rs
+++ src/merge/solution.rs
@@ -1,7 +1,25 @@
 use crate::*;
 
 impl Solution {
-    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+    pub fn merge_intervals(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut intervals = intervals;
+        intervals.sort();
+        let mut output: Vec<Vec<i32>> = vec![];
+        for interval in intervals.into_iter() {
+            if let Some(last) = output.last_mut() {
+                if interval[0] > last[1] {
+                    output.push(interval);
+                } else {
+                    last[1] = last[1].max(interval[1]);
+                }
+            } else {
+                output.push(interval);
+            }
+        }
+        output
+    }
+
+    pub fn merge_sorted_array(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
         let mut m = m as usize;
         let mut n = n as usize;
         let mut idx = nums1.len();
--- src/merge/tests.rs
+++ src/merge/tests.rs
@@ -4,7 +4,7 @@ use crate::*;
 fn case_1() {
     let mut nums1 = vec![1, 2, 3, 0, 0, 0];
     let mut nums2 = vec![2, 5, 6];
-    Solution::merge(&mut nums1, 3, &mut nums2, 3);
+    Solution::merge_sorted_array(&mut nums1, 3, &mut nums2, 3);
     assert_eq!(nums1, vec![1, 2, 2, 3, 5, 6]);
 }
 
@@ -12,6 +12,30 @@ fn case_1() {
 fn case_2() {
     let mut nums1 = vec![1];
     let mut nums2 = vec![];
-    Solution::merge(&mut nums1, 1, &mut nums2, 0);
+    Solution::merge_sorted_array(&mut nums1, 1, &mut nums2, 0);
     assert_eq!(nums1, vec![1]);
+}
+
+#[test]
+fn case_3() {
+    assert_eq!(
+        Solution::merge_intervals(vec![vec![1, 3], vec![2, 6], vec![8, 10], vec![15, 18]]),
+        vec![vec![1, 6], vec![8, 10], vec![15, 18]]
+    );
+}
+
+#[test]
+fn case_4() {
+    assert_eq!(
+        Solution::merge_intervals(vec![vec![1, 4], vec![4, 5]]),
+        vec![vec![1, 5]]
+    );
+}
+
+#[test]
+fn case_5() {
+    assert_eq!(
+        Solution::merge_intervals(vec![vec![1, 4], vec![2, 3]]),
+        vec![vec![1, 4]]
+    );
 }

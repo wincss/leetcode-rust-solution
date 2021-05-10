5b36788cf3cd42081b90441acd930518297dd0b7
wincss <wincss@gmail.com>
2021-05-10 16:03:18 +0800

    add test for group_anagrams

--- src/group_anagrams/tests.rs
+++ src/group_anagrams/tests.rs
@@ -1,5 +1,19 @@
+use crate::*;
+
 #[test]
-#[ignore]
 fn case_1() {
-    todo!();
+    let mut output =
+        Solution::group_anagrams(to_string_vec(&["eat", "tea", "tan", "ate", "nat", "bat"]));
+    for item in output.iter_mut() {
+        item.sort();
+    }
+    output.sort();
+    assert_eq!(
+        output,
+        vec![
+            to_string_vec(&["ate", "eat", "tea"]),
+            to_string_vec(&["bat"]),
+            to_string_vec(&["nat", "tan"]),
+        ]
+    );
 }

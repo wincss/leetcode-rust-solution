c21eb37f40b0813d67ccab3da5aa2e451c1a9c0f
wincss <wincss@gmail.com>
2021-05-11 15:11:58 +0800

    add decode (1734)

--- src/decode/solution.rs
+++ src/decode/solution.rs
@@ -1,12 +1,23 @@
 use crate::*;
 
 impl Solution {
-    pub fn decode(encoded: Vec<i32>, first: i32) -> Vec<i32> {
+    pub fn decode_1720(encoded: Vec<i32>, first: i32) -> Vec<i32> {
         let mut result = vec![first];
         result.extend(encoded.into_iter().scan(first, |a, b| {
             *a ^= b;
             Some(*a)
         }));
         result
+    }
+    pub fn decode_1734(encoded: Vec<i32>) -> Vec<i32> {
+        let n = encoded.len() + 1;
+        let mut first = 0;
+        for i in 1..=n {
+            first ^= i as i32;
+            if i < n - 1 && i & 1 == 1 {
+                first ^= encoded[i];
+            }
+        }
+        Self::decode_1720(encoded, first)
     }
 }
--- src/decode/tests.rs
+++ src/decode/tests.rs
@@ -2,10 +2,23 @@ use crate::*;
 
 #[test]
 fn case_1() {
-    assert_eq!(Solution::decode(vec![1, 2, 3], 1), vec![1, 0, 2, 1]);
+    assert_eq!(Solution::decode_1720(vec![1, 2, 3], 1), vec![1, 0, 2, 1]);
 }
 
 #[test]
 fn case_2() {
-    assert_eq!(Solution::decode(vec![6, 2, 7, 3], 4), vec![4, 2, 0, 7, 4]);
+    assert_eq!(
+        Solution::decode_1720(vec![6, 2, 7, 3], 4),
+        vec![4, 2, 0, 7, 4]
+    );
+}
+
+#[test]
+fn case_3() {
+    assert_eq!(Solution::decode_1734(vec![3, 1]), vec![1, 2, 3]);
+}
+
+#[test]
+fn case_4() {
+    assert_eq!(Solution::decode_1734(vec![6, 5, 4, 6]), vec![2, 4, 1, 5, 3]);
 }

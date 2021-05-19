be407eecb9c296e424d7a0649d922819b3517229
wincss <wincss@gmail.com>
2021-05-20 01:53:18 +0800

    add top_k_frequent (692)

--- src/top_k_frequent/solution.rs
+++ src/top_k_frequent/solution.rs
@@ -2,14 +2,21 @@ use crate::*;
 
 use std::collections::HashMap;
 impl Solution {
-    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    pub fn top_k_frequent<T>(nums: Vec<T>, k: i32) -> Vec<T>
+    where
+        T: Clone + std::hash::Hash + std::cmp::Eq + std::cmp::PartialOrd,
+    {
         let mut count = HashMap::new();
-        for i in nums.into_iter() {
-            let v = count.entry(i).or_insert(0);
+        for i in nums.iter() {
+            let v = count.entry(i.clone()).or_insert(0);
             *v += 1;
         }
-        let mut keys = count.keys().map(|v| *v).collect::<Vec<i32>>();
-        keys.sort_unstable_by_key(|v| count[v]);
-        Vec::from(&keys[keys.len() - k as usize..])
+        let mut keys = count.keys().map(|v| v.clone()).collect::<Vec<T>>();
+        keys.sort_unstable_by(|v1, v2| match count[v1].partial_cmp(&count[v2]).unwrap() {
+            std::cmp::Ordering::Equal => v1.partial_cmp(&v2).unwrap(),
+            std::cmp::Ordering::Greater => std::cmp::Ordering::Less,
+            std::cmp::Ordering::Less => std::cmp::Ordering::Greater,
+        });
+        Vec::from(&keys[..k as usize])
     }
 }
--- src/top_k_frequent/tests.rs
+++ src/top_k_frequent/tests.rs
@@ -11,3 +11,25 @@ fn case_1() {
 fn case_2() {
     assert_eq!(Solution::top_k_frequent(vec![1], 1), vec![1]);
 }
+
+#[test]
+fn case_3() {
+    assert_eq!(
+        Solution::top_k_frequent(
+            to_string_vec(&["i", "love", "leetcode", "i", "love", "coding"]),
+            2
+        ),
+        to_string_vec(&["i", "love"])
+    );
+}
+
+#[test]
+fn case_4() {
+    assert_eq!(
+        Solution::top_k_frequent(
+            to_string_vec(&["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"]),
+            4
+        ),
+        to_string_vec(&["the", "is", "sunny", "day"])
+    );
+}

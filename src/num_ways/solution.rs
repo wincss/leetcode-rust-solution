d769e56697445852f569564277f52ba222d9a8be
wincss <wincss@gmail.com>
2021-07-01 00:19:28 +0800

    add num_ways (lcp07)

--- src/num_ways/solution.rs
+++ src/num_ways/solution.rs
@@ -1,7 +1,28 @@
 use crate::*;
 
+use std::collections::HashSet;
 impl Solution {
-    pub fn num_ways(steps: i32, arr_len: i32) -> i32 {
+    pub fn num_ways_lcp_07(n: i32, relation: Vec<Vec<i32>>, k: i32) -> i32 {
+        let n = n as usize;
+        let mut path = vec![HashSet::new(); n];
+        for rel in relation.into_iter() {
+            path[rel[0] as usize].insert(rel[1] as usize);
+        }
+        let mut msg = vec![0; n];
+        msg[0] = 1;
+        for _ in 0..k {
+            let mut new_msg = vec![0; n];
+            for from in 0..n {
+                for &to in path[from].iter() {
+                    new_msg[to] += msg[from];
+                }
+            }
+            msg = new_msg;
+        }
+        msg[n - 1]
+    }
+
+    pub fn num_ways_1269(steps: i32, arr_len: i32) -> i32 {
         const MOD: u32 = 1000000007;
         let n = arr_len.min(steps) as usize;
         let steps = steps as usize;
--- src/num_ways/tests.rs
+++ src/num_ways/tests.rs
@@ -2,20 +2,48 @@ use crate::*;
 
 #[test]
 fn case_1() {
-    assert_eq!(Solution::num_ways(3, 2), 4);
+    assert_eq!(Solution::num_ways_1269(3, 2), 4);
 }
 
 #[test]
 fn case_2() {
-    assert_eq!(Solution::num_ways(2, 4), 2);
+    assert_eq!(Solution::num_ways_1269(2, 4), 2);
 }
 
 #[test]
 fn case_3() {
-    assert_eq!(Solution::num_ways(4, 2), 8);
+    assert_eq!(Solution::num_ways_1269(4, 2), 8);
 }
 
 #[test]
 fn case_4() {
-    assert_eq!(Solution::num_ways(27, 7), 127784505);
+    assert_eq!(Solution::num_ways_1269(27, 7), 127784505);
+}
+
+#[test]
+fn case_lcp_07_1() {
+    assert_eq!(
+        Solution::num_ways_lcp_07(
+            5,
+            vec![
+                vec![0, 2],
+                vec![2, 1],
+                vec![3, 4],
+                vec![2, 3],
+                vec![1, 4],
+                vec![2, 0],
+                vec![0, 4]
+            ],
+            3
+        ),
+        3
+    );
+}
+
+#[test]
+fn case_lcp_07_2() {
+    assert_eq!(
+        Solution::num_ways_lcp_07(3, vec![vec![0, 2], vec![2, 1],], 2),
+        0
+    );
 }

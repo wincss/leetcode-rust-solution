e6afedb7830fa38844a979ad97a9fa1858b1e9a7
wincss <wincss@gmail.com>
2021-07-02 01:42:30 +0800

    optimize: use counting sort

--- src/max_ice_cream/solution.rs
+++ src/max_ice_cream/solution.rs
@@ -13,4 +13,26 @@ impl Solution {
         }
         costs.len() as i32
     }
+
+    pub fn max_ice_cream_by_counting_sort(costs: Vec<i32>, coins: i32) -> i32 {
+        const MAX_COST: usize = 100000;
+        let mut count = vec![0; MAX_COST + 1];
+        for i in costs {
+            count[i as usize] += 1;
+        }
+        let mut remain = coins as usize;
+        let mut amount = 0;
+        for i in 1..=MAX_COST {
+            if count[i] == 0 {
+                continue;
+            }
+            let current = count[i].min(remain / i);
+            amount += current;
+            remain -= i * current;
+            if current < count[i] {
+                break;
+            }
+        }
+        amount as i32
+    }
 }
--- src/max_ice_cream/tests.rs
+++ src/max_ice_cream/tests.rs
@@ -3,14 +3,26 @@ use crate::*;
 #[test]
 fn case_1() {
     assert_eq!(Solution::max_ice_cream(vec![1, 3, 2, 4, 1], 7), 4);
+    assert_eq!(
+        Solution::max_ice_cream_by_counting_sort(vec![1, 3, 2, 4, 1], 7),
+        4
+    );
 }
 
 #[test]
 fn case_2() {
     assert_eq!(Solution::max_ice_cream(vec![10, 6, 8, 7, 7, 8], 5), 0);
+    assert_eq!(
+        Solution::max_ice_cream_by_counting_sort(vec![10, 6, 8, 7, 7, 8], 5),
+        0
+    );
 }
 
 #[test]
 fn case_3() {
     assert_eq!(Solution::max_ice_cream(vec![1, 6, 3, 1, 2, 5], 20), 6);
+    assert_eq!(
+        Solution::max_ice_cream_by_counting_sort(vec![1, 6, 3, 1, 2, 5], 20),
+        6
+    );
 }

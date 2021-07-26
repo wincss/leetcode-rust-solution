c5b078beb061c78bf31838ea0b81270f2876e041
wincss <wincss@gmail.com>
2021-07-26 18:51:51 +0800

    add min_operations (1713)

--- src/min_operations/solution.rs
+++ src/min_operations/solution.rs
@@ -1,7 +1,28 @@
 use crate::*;
 
+use std::collections::HashMap;
 impl Solution {
-    pub fn min_operations(nums: Vec<i32>) -> i32 {
+    pub fn min_operations_1713(target: Vec<i32>, arr: Vec<i32>) -> i32 {
+        let n = target.len();
+        let mut position = HashMap::new();
+        for (idx, v) in target.into_iter().enumerate() {
+            position.insert(v, idx);
+        }
+        let mut d = vec![];
+        for v in arr.into_iter() {
+            if let Some(&idx) = position.get(&v) {
+                let inpos = d.binary_search(&idx).unwrap_or_else(|x| x);
+                if inpos < d.len() {
+                    d[inpos] = idx;
+                } else {
+                    d.push(idx);
+                }
+            }
+        }
+        (n - d.len()) as i32
+    }
+
+    pub fn min_operations_1827(nums: Vec<i32>) -> i32 {
         let mut op = 0;
         let mut last = 0;
         for i in nums {
--- src/min_operations/tests.rs
+++ src/min_operations/tests.rs
@@ -2,15 +2,31 @@ use crate::*;
 
 #[test]
 fn case_1() {
-    assert_eq!(Solution::min_operations(vec![1, 1, 1]), 3);
+    assert_eq!(Solution::min_operations_1827(vec![1, 1, 1]), 3);
 }
 
 #[test]
 fn case_2() {
-    assert_eq!(Solution::min_operations(vec![1, 5, 2, 4, 1]), 14);
+    assert_eq!(Solution::min_operations_1827(vec![1, 5, 2, 4, 1]), 14);
 }
 
 #[test]
 fn case_3() {
-    assert_eq!(Solution::min_operations(vec![8]), 0);
+    assert_eq!(Solution::min_operations_1827(vec![8]), 0);
+}
+
+#[test]
+fn case_4() {
+    assert_eq!(
+        Solution::min_operations_1713(vec![5, 1, 3], vec![9, 4, 2, 3, 4]),
+        2
+    );
+}
+
+#[test]
+fn case_5() {
+    assert_eq!(
+        Solution::min_operations_1713(vec![6, 4, 8, 1, 3, 2], vec![4, 7, 6, 2, 3, 8, 6, 1]),
+        3
+    );
 }

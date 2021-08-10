aedf73a392aaa5adf571a1bb4844cafa789e6067
wincss <wincss@gmail.com>
2021-08-11 02:28:26 +0800

    add number_of_arithmetic_slices (446)

--- src/number_of_arithmetic_slices/solution.rs
+++ src/number_of_arithmetic_slices/solution.rs
@@ -1,7 +1,26 @@
 use crate::*;
 
+use std::collections::HashMap;
 impl Solution {
-    pub fn number_of_arithmetic_slices(nums: Vec<i32>) -> i32 {
+    pub fn number_of_arithmetic_slices_446(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        if n < 3 {
+            return 0;
+        }
+        let mut dp = vec![HashMap::new(); n];
+        let mut result = 0;
+        for i in 0..n {
+            for j in 0..i {
+                if let Some(d) = nums[i].checked_sub(nums[j]) {
+                    let count = *dp[j].entry(d).or_insert(0);
+                    result += count;
+                    *dp[i].entry(d).or_insert(0) += count + 1;
+                }
+            }
+        }
+        result
+    }
+    pub fn number_of_arithmetic_slices_413(nums: Vec<i32>) -> i32 {
         let n = nums.len();
         if n < 3 {
             return 0;
--- src/number_of_arithmetic_slices/tests.rs
+++ src/number_of_arithmetic_slices/tests.rs
@@ -2,18 +2,44 @@ use crate::*;
 
 #[test]
 fn case_1() {
-    assert_eq!(Solution::number_of_arithmetic_slices(vec![1, 2, 3, 4]), 3);
+    assert_eq!(
+        Solution::number_of_arithmetic_slices_413(vec![1, 2, 3, 4]),
+        3
+    );
 }
 
 #[test]
 fn case_2() {
-    assert_eq!(Solution::number_of_arithmetic_slices(vec![1]), 0);
+    assert_eq!(Solution::number_of_arithmetic_slices_413(vec![1]), 0);
 }
 
 #[test]
 fn case_3() {
     assert_eq!(
-        Solution::number_of_arithmetic_slices(vec![1, 2, 3, 5, 7]),
+        Solution::number_of_arithmetic_slices_413(vec![1, 2, 3, 5, 7]),
         2
+    );
+}
+
+#[test]
+fn case_4() {
+    assert_eq!(
+        Solution::number_of_arithmetic_slices_446(vec![2, 4, 6, 8, 10]),
+        7
+    );
+}
+
+#[test]
+fn case_5() {
+    assert_eq!(
+        Solution::number_of_arithmetic_slices_446(vec![7, 7, 7, 7, 7]),
+        16
+    );
+}
+#[test]
+fn case_6() {
+    assert_eq!(
+        Solution::number_of_arithmetic_slices_446(vec![0, 2000000000, -294967296]),
+        0
     );
 }

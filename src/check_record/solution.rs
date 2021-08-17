502fa440f5e07d0531426faf1e7a015fb6b2af02
wincss <wincss@gmail.com>
2021-08-18 00:33:23 +0800

    add check_record (552)

--- src/check_record/solution.rs
+++ src/check_record/solution.rs
@@ -1,7 +1,25 @@
 use crate::*;
 
 impl Solution {
-    pub fn check_record(s: String) -> bool {
+    pub fn check_record_552(n: i32) -> i32 {
+        const MOD: i64 = 1000000007;
+        // state:
+        // .0 .1 .2 => absent 0, late 0, 1, 2
+        // .3 .4 .5 => absent 1, late 0, 1, 2
+        let mut state = (1, 0, 0, 0, 0, 0);
+        for _ in 0..n {
+            state = (
+                (state.0 + state.1 + state.2) % MOD,
+                state.0,
+                state.1,
+                (state.0 + state.1 + state.2 + state.3 + state.4 + state.5) % MOD,
+                state.3,
+                state.4,
+            );
+        }
+        ((state.0 + state.1 + state.2 + state.3 + state.4 + state.5) % MOD) as i32
+    }
+    pub fn check_record_551(s: String) -> bool {
         let mut absent = 0;
         let mut late = 0;
         let mut continous_late = false;
--- src/check_record/tests.rs
+++ src/check_record/tests.rs
@@ -2,10 +2,30 @@ use crate::*;
 
 #[test]
 fn case_1() {
-    assert!(Solution::check_record("PPALLP".to_string()));
+    assert!(Solution::check_record_551("PPALLP".to_string()));
 }
 
 #[test]
 fn case_2() {
-    assert!(!Solution::check_record("PPALLL".to_string()));
+    assert!(!Solution::check_record_551("PPALLL".to_string()));
+}
+
+#[test]
+fn case_3() {
+    assert_eq!(Solution::check_record_552(1), 3);
+}
+
+#[test]
+fn case_4() {
+    assert_eq!(Solution::check_record_552(2), 8);
+}
+
+#[test]
+fn case_5() {
+    assert_eq!(Solution::check_record_552(10101), 183236316);
+}
+
+#[test]
+fn case_6() {
+    assert_eq!(Solution::check_record_552(100000), 749184020);
 }

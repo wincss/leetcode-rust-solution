30e7b729a9fbd8f2aaa6df3d01128e5393df7d56
wincss <wincss@gmail.com>
2021-08-19 01:54:48 +0800

    rustfmt

--- src/backspace_compare/tests.rs
+++ src/backspace_compare/tests.rs
@@ -2,31 +2,19 @@ use crate::*;
 
 #[test]
 fn case_1() {
-    assert!(Solution::backspace_compare(
-        s!("ab#c"),
-        s!("ad#c")
-    ));
+    assert!(Solution::backspace_compare(s!("ab#c"), s!("ad#c")));
 }
 
 #[test]
 fn case_2() {
-    assert!(Solution::backspace_compare(
-        s!("ab##"),
-        s!("c#d#")
-    ));
+    assert!(Solution::backspace_compare(s!("ab##"), s!("c#d#")));
 }
 #[test]
 fn case_3() {
-    assert!(Solution::backspace_compare(
-        s!("a##c"),
-        s!("#a#c")
-    ));
+    assert!(Solution::backspace_compare(s!("a##c"), s!("#a#c")));
 }
 
 #[test]
 fn case_4() {
-    assert!(!Solution::backspace_compare(
-        s!("a#c"),
-        s!("b")
-    ));
+    assert!(!Solution::backspace_compare(s!("a#c"), s!("b")));
 }

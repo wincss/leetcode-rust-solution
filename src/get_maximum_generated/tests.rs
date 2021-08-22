2219ff858343726271aa57773d9e44543da02431
wincss <wincss@gmail.com>
2021-08-23 01:10:27 +0800

    add tests for get_maximum_generated

--- src/get_maximum_generated/tests.rs
+++ src/get_maximum_generated/tests.rs
@@ -2,6 +2,25 @@ use crate::*;
 
 #[test]
 fn case_1() {
-    // Solution::get_maximum_generated
-    todo!();
+    assert_eq!(Solution::get_maximum_generated(0), 0);
+}
+
+#[test]
+fn case_2() {
+    assert_eq!(Solution::get_maximum_generated(1), 1);
+}
+
+#[test]
+fn case_3() {
+    assert_eq!(Solution::get_maximum_generated(2), 1);
+}
+
+#[test]
+fn case_4() {
+    assert_eq!(Solution::get_maximum_generated(3), 2);
+}
+
+#[test]
+fn case_5() {
+    assert_eq!(Solution::get_maximum_generated(7), 3);
 }

c69955696259c4724c947117a6ebc545d2db7c99
wincss <wincss@gmail.com>
2021-09-11 00:37:04 +0800

    optimize: remove digits

--- src/find_integers/solution.rs
+++ src/find_integers/solution.rs
@@ -3,47 +3,41 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn find_integers(num: i32) -> i32 {
-        let mut n = num;
-        let mut digits = vec![];
-        while n > 0 {
-            digits.push(n & 1);
-            n >>= 1;
-        }
-        digits.reverse();
-        let n = digits.len();
+        let n = 32 - num.leading_zeros() as usize;
         fn dp(
             k: usize,
             n: usize,
-            digits: &Vec<i32>,
+            num: i32,
             ubound: bool,
-            state: i32,
-            save: &mut HashMap<(usize, bool, i32), i32>,
+            continuous_ones: u8,
+            save: &mut HashMap<(usize, bool, u8), i32>,
         ) -> i32 {
             if k == n {
-                return if state == 2 { 0 } else { 1 };
+                return if continuous_ones == 2 { 0 } else { 1 };
             }
-            if save.contains_key(&(k, ubound, state)) {
-                return save[&(k, ubound, state)];
+            if save.contains_key(&(k, ubound, continuous_ones)) {
+                return save[&(k, ubound, continuous_ones)];
             }
             let mut result = 0;
-            let h = if ubound { digits[k] } else { 1 };
+            let h = if ubound && num & (1 << (n - 1 - k)) == 0 {
+                0
+            } else {
+                1
+            };
             for i in 0..=h {
                 let ubound = ubound && (i == h);
-                let state = if state == 2 {
-                    2
-                } else if state == 1 && i == 1 {
-                    2
-                } else if state == 0 && i == 1 {
-                    1
-                } else {
-                    0
+                let continuous_ones = match (continuous_ones, i) {
+                    (2, _) => 2,
+                    (1, 1) => 2,
+                    (0, 1) => 1,
+                    _ => 0,
                 };
-                result += dp(k + 1, n, digits, ubound, state, save);
+                result += dp(k + 1, n, num, ubound, continuous_ones, save);
             }
-            save.insert((k, ubound, state), result);
+            save.insert((k, ubound, continuous_ones), result);
             result
         }
         let mut save = HashMap::new();
-        dp(0, n, &digits, true, 0, &mut save)
+        dp(0, n, num, true, 0, &mut save)
     }
 }

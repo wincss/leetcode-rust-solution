e96edd76b917c37fed6183ab91d79191250d2c24
wincss <wincss@gmail.com>
2021-09-28 00:50:17 +0800

    add path_sum (437)

--- src/path_sum/solution.rs
+++ src/path_sum/solution.rs
@@ -2,8 +2,11 @@ use crate::*;
 
 use std::cell::RefCell;
 use std::rc::Rc;
+
+use std::collections::HashMap;
+
 impl Solution {
-    pub fn path_sum(root: Option<Rc<RefCell<TreeNode>>>, sum: i32) -> Vec<Vec<i32>> {
+    pub fn path_sum_113(root: Option<Rc<RefCell<TreeNode>>>, sum: i32) -> Vec<Vec<i32>> {
         fn walk(
             root: Option<Rc<RefCell<TreeNode>>>,
             remain: i32,
@@ -29,5 +32,43 @@ impl Solution {
         let mut current = vec![];
         walk(root, sum, &mut current, &mut result);
         result
+    }
+
+    pub fn path_sum_437(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> i32 {
+        fn walk(
+            root: Option<Rc<RefCell<TreeNode>>>,
+            target_sum: i64,
+            current_sum: i64,
+            prefix: &mut HashMap<i64, i32>,
+            count: &mut i32,
+        ) {
+            if root.is_none() {
+                return;
+            }
+            let node = root.as_ref().unwrap().borrow();
+            let current_sum = current_sum + node.val as i64;
+            *count += *prefix.get(&(current_sum - target_sum)).unwrap_or(&0);
+            *prefix.entry(current_sum).or_insert(0) += 1;
+            walk(
+                node.left.as_ref().map(Rc::clone),
+                target_sum,
+                current_sum,
+                prefix,
+                count,
+            );
+            walk(
+                node.right.as_ref().map(Rc::clone),
+                target_sum,
+                current_sum,
+                prefix,
+                count,
+            );
+            prefix.entry(current_sum).and_modify(|v| *v -= 1);
+        }
+        let mut count = 0;
+        let mut prefix = HashMap::new();
+        prefix.insert(0, 1);
+        walk(root, target_sum as i64, 0, &mut prefix, &mut count);
+        count
     }
 }
--- src/path_sum/tests.rs
+++ src/path_sum/tests.rs
@@ -4,7 +4,7 @@ use serde_json::json;
 
 #[test]
 fn case_1() {
-    let mut output = Solution::path_sum(
+    let mut output = Solution::path_sum_113(
         TreeNode::from_json(json!([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1])),
         22,
     );
@@ -16,9 +16,53 @@ fn case_1() {
 
 #[test]
 fn case_2() {
-    let mut output = Solution::path_sum(TreeNode::from_json(json!([-2, null, -3])), -5);
+    let mut output = Solution::path_sum_113(TreeNode::from_json(json!([-2, null, -3])), -5);
     output.sort();
     let mut expect = vec![vec![-2, -3]];
     expect.sort();
     assert_eq!(output, expect);
+}
+
+#[test]
+fn case_437_1() {
+    assert_eq!(
+        Solution::path_sum_437(
+            TreeNode::from_json(json!([10, 5, -3, 3, 2, null, 11, 3, -2, null, 1])),
+            8
+        ),
+        3
+    );
+}
+
+#[test]
+fn case_437_2() {
+    assert_eq!(
+        Solution::path_sum_437(
+            TreeNode::from_json(json!([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1])),
+            22
+        ),
+        3
+    );
+}
+
+#[test]
+fn case_437_3() {
+    assert_eq!(
+        Solution::path_sum_437(
+            TreeNode::from_json(json!([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1])),
+            22
+        ),
+        3
+    );
+}
+
+#[test]
+fn case_437_4() {
+    assert_eq!(
+        Solution::path_sum_437(
+            TreeNode::from_json(json!([10, 5, -3, 3, 24, null, 11, 3, -2, null, 1])),
+            21
+        ),
+        1
+    );
 }

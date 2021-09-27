f7a900f1d2813d58d0288b404e31c8e0dbdc2cef
wincss <wincss@gmail.com>
2021-09-28 00:09:59 +0800

    add num_decodings (639)

--- src/num_decodings/solution.rs
+++ src/num_decodings/solution.rs
@@ -1,7 +1,7 @@
 use crate::*;
 
 impl Solution {
-    pub fn num_decodings(s: String) -> i32 {
+    pub fn num_decodings_91(s: String) -> i32 {
         let s: Vec<char> = s.chars().collect();
         let n = s.len();
         let mut dp1 = 1;
@@ -18,5 +18,31 @@ impl Solution {
             // println!("dp1={}, dp2={}", dp1, dp2);
         }
         dp1
+    }
+
+    pub fn num_decodings_639(s: String) -> i32 {
+        const MOD: i64 = 1000000007;
+        let s: Vec<char> = s.chars().collect();
+        let n = s.len();
+        let mut dp1 = 1;
+        let mut dp2 = 0;
+
+        for i in (0..n).rev() {
+            let tmp = match (s[i], s.get(i + 1).unwrap_or(&'A').clone()) {
+                ('*', '*') => dp1 * 9 + dp2 * 9 + dp2 * 6,
+                ('*', x) if x <= '6' => dp1 * 9 + dp2 + dp2,
+                ('*', _) => dp1 * 9 + dp2,
+                ('0', _) => 0,
+                ('1', '*') => dp1 + dp2 * 9,
+                ('1', _) => dp1 + dp2,
+                ('2', '*') => dp1 + dp2 * 6,
+                ('2', x) if x <= '6' => dp1 + dp2,
+                _ => dp1,
+            };
+            dp2 = dp1;
+            dp1 = tmp % MOD;
+            // println!("dp1={}, dp2={}", dp1, dp2);
+        }
+        dp1 as i32
     }
 }
--- src/num_decodings/tests.rs
+++ src/num_decodings/tests.rs
@@ -2,30 +2,64 @@ use crate::*;
 
 #[test]
 fn case_1() {
-    assert_eq!(Solution::num_decodings("12".to_string()), 2);
+    assert_eq!(Solution::num_decodings_91("12".to_string()), 2);
 }
 
 #[test]
 fn case_2() {
-    assert_eq!(Solution::num_decodings("226".to_string()), 3);
+    assert_eq!(Solution::num_decodings_91("226".to_string()), 3);
 }
 
 #[test]
 fn case_3() {
-    assert_eq!(Solution::num_decodings("0".to_string()), 0);
+    assert_eq!(Solution::num_decodings_91("0".to_string()), 0);
 }
 
 #[test]
 fn case_4() {
-    assert_eq!(Solution::num_decodings("06".to_string()), 0);
+    assert_eq!(Solution::num_decodings_91("06".to_string()), 0);
 }
 
 #[test]
 fn case_5() {
-    assert_eq!(Solution::num_decodings("1".to_string()), 1);
+    assert_eq!(Solution::num_decodings_91("1".to_string()), 1);
 }
 
 #[test]
 fn case_6() {
-    assert_eq!(Solution::num_decodings("11".to_string()), 2);
+    assert_eq!(Solution::num_decodings_91("11".to_string()), 2);
+}
+
+#[test]
+fn case_639_1() {
+    assert_eq!(Solution::num_decodings_639(s!("*")), 9);
+}
+
+#[test]
+fn case_639_2() {
+    assert_eq!(Solution::num_decodings_639(s!("1*")), 18);
+}
+
+#[test]
+fn case_639_3() {
+    assert_eq!(Solution::num_decodings_639(s!("2*")), 15);
+}
+
+#[test]
+fn case_639_4() {
+    assert_eq!(Solution::num_decodings_639(s!("2*5")), 17);
+}
+
+#[test]
+fn case_639_5() {
+    assert_eq!(Solution::num_decodings_639(s!("2*5*8")), 170);
+}
+
+#[test]
+fn case_639_6() {
+    assert_eq!(Solution::num_decodings_639(s!("**")), 96);
+}
+#[test]
+fn case_639_7() {
+    assert_eq!(Solution::num_decodings_639(s!("*********")), 291868912);
 }

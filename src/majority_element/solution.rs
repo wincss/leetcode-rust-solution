3df8de720932b7022a6231eed5177f67791a01eb
wincss <wincss@gmail.com>
2021-10-22 14:37:12 +0800

    add majority_element (229)

--- src/majority_element/solution.rs
+++ src/majority_element/solution.rs
@@ -1,7 +1,39 @@
 use crate::*;
 
 impl Solution {
-    pub fn majority_element(nums: Vec<i32>) -> i32 {
+    pub fn majority_element_one_third(nums: Vec<i32>) -> Vec<i32> {
+        let mut last1 = 0;
+        let mut last2 = 0;
+        let mut vote1 = 0;
+        let mut vote2 = 0;
+        for &i in nums.iter() {
+            if i == last1 {
+                vote1 += 1;
+            } else if i == last2 {
+                vote2 += 1;
+            } else if vote1 == 0 {
+                vote1 = 1;
+                last1 = i;
+            } else if vote2 == 0 {
+                vote2 = 1;
+                last2 = i;
+            } else {
+                vote1 -= 1;
+                vote2 -= 1;
+            }
+        }
+        let count1 = nums.iter().filter(|&&v| v == last1).count();
+        let count2 = nums.iter().filter(|&&v| v == last2).count();
+        let mut result = vec![];
+        if vote1 > 0 && count1 > nums.len() / 3 {
+            result.push(last1);
+        }
+        if vote2 > 0 && count2 > nums.len() / 3 {
+            result.push(last2);
+        }
+        result
+    }
+    pub fn majority_element_half(nums: Vec<i32>) -> i32 {
         let mut last = 0;
         let mut vote = 0;
         for &i in nums.iter() {
--- src/majority_element/tests.rs
+++ src/majority_element/tests.rs
@@ -3,22 +3,45 @@ use crate::*;
 #[test]
 fn case_1() {
     assert_eq!(
-        Solution::majority_element(vec![1, 2, 5, 9, 5, 9, 5, 5, 5]),
+        Solution::majority_element_half(vec![1, 2, 5, 9, 5, 9, 5, 5, 5]),
         5
     );
 }
 
 #[test]
 fn case_2() {
-    assert_eq!(Solution::majority_element(vec![3, 3, 4]), 3);
+    assert_eq!(Solution::majority_element_half(vec![3, 3, 4]), 3);
 }
 
 #[test]
 fn case_3() {
-    assert_eq!(Solution::majority_element(vec![3, 2]), -1);
+    assert_eq!(Solution::majority_element_half(vec![3, 2]), -1);
 }
 
 #[test]
 fn case_4() {
-    assert_eq!(Solution::majority_element(vec![1]), 1);
+    assert_eq!(Solution::majority_element_half(vec![1]), 1);
+}
+
+fn check_output_one_third(input: Vec<i32>, expect: Vec<i32>) {
+    let mut output = Solution::majority_element_one_third(input);
+    let mut expect = expect;
+    output.sort();
+    expect.sort();
+    assert_eq!(output, expect);
+}
+
+#[test]
+fn case_one_third_1() {
+    check_output_one_third(vv![3, 2, 3], vv![3]);
+}
+
+#[test]
+fn case_one_third_2() {
+    check_output_one_third(vv![1], vv![1]);
+}
+
+#[test]
+fn case_one_third_3() {
+    check_output_one_third(vv![1, 1, 1, 3, 3, 2, 2, 2], vv![1, 2]);
 }

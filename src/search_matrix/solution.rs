eda3042bc9b12fdd67e9d4db86aa9b686eec8dea
wincss <wincss@gmail.com>
2021-10-25 01:31:58 +0800

    add search_matrix (240)

--- src/search_matrix/solution.rs
+++ src/search_matrix/solution.rs
@@ -1,6 +1,28 @@
 use crate::*;
 
 impl Solution {
+    pub fn search_matrix_ii(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        let n = matrix.len();
+        assert!(n > 0);
+        let m = matrix[0].len();
+        let mut x = 0;
+        let mut y = m - 1;
+        while x < n {
+            if matrix[x][y] == target {
+                return true;
+            }
+            if matrix[x][y] > target {
+                if y > 0 {
+                    y -= 1;
+                } else {
+                    break;
+                }
+            } else {
+                x += 1;
+            }
+        }
+        false
+    }
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
         let n = matrix.len();
         assert!(n > 0);

ab4ee62a5febfdbf7adb72325daa867dd3b889a6
wincss <wincss@gmail.com>
2021-11-15 13:51:27 +0800

    add class interface for union_find

--- src/common/algorithms/union_find.rs
+++ src/common/algorithms/union_find.rs
@@ -1,5 +1,29 @@
 use std::collections::HashMap;
 
+pub struct UnionFind<T> {
+    parent: HashMap<T, T>,
+    size: HashMap<T, usize>,
+}
+
+impl<T> UnionFind<T>
+where
+    T: std::cmp::Eq + std::hash::Hash + Clone,
+{
+    pub fn new() -> Self {
+        Self {
+            parent: HashMap::new(),
+            size: HashMap::new(),
+        }
+    }
+    pub fn find(&mut self, x: &T) -> T {
+        find(x, &mut self.parent, &mut self.size)
+    }
+
+    pub fn union(&mut self, x: &T, y: &T) -> bool {
+        union(x, y, &mut self.parent, &mut self.size)
+    }
+}
+
 pub fn find<T>(x: &T, parent: &mut HashMap<T, T>, size: &mut HashMap<T, usize>) -> T
 where
     T: std::cmp::Eq + std::hash::Hash + Clone,

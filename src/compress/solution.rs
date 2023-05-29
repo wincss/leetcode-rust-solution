3fad48820b08155c8e42bf4a5e99671138f33963
wincss <wincss@gmail.com>
2023-05-29 17:42:43 +0800

    refactor: reduce redundancy

--- src/compress/solution.rs
+++ src/compress/solution.rs
@@ -8,54 +8,28 @@ impl Solution {
         let mut last = 'a';
         let mut count = 0;
 
-        while rp < n {
-            if chars[rp] == last {
+        while rp <= n {
+            if rp < n && chars[rp] == last {
                 count += 1;
                 rp += 1;
                 continue;
             }
             if count > 0 {
-                let length = if count >= 1000 {
-                    4
-                } else if count >= 100 {
-                    3
-                } else if count >= 10 {
-                    2
-                } else if count >= 2 {
-                    1
-                } else {
-                    0
-                };
                 chars[wp] = last;
-                for i in (wp + 1..=wp + length).rev() {
-                    chars[i] = (count % 10 + 48) as u8 as char;
-                    count /= 10;
+                wp += 1;
+                if count > 1 {
+                    for c in count.to_string().chars() {
+                        chars[wp] = c;
+                        wp += 1;
+                    }
                 }
-                wp += 1 + length;
             }
-            count = 1;
-            last = chars[rp];
+            if rp < n {
+                count = 1;
+                last = chars[rp];
+            }
             // println!("{:?} {} {}", chars, rp, wp);
             rp += 1;
-        }
-        if count > 0 {
-            let length = if count >= 1000 {
-                4
-            } else if count >= 100 {
-                3
-            } else if count >= 10 {
-                2
-            } else if count >= 2 {
-                1
-            } else {
-                0
-            };
-            chars[wp] = last;
-            for i in (wp + 1..=wp + length).rev() {
-                chars[i] = (count % 10 + 48) as u8 as char;
-                count /= 10;
-            }
-            wp += 1 + length;
         }
         // println!("{:?} {} {}", chars, rp, wp);
         wp as i32
